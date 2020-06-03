42e4522e928a92db75ad4c985049fec075fe266c
SAI RITHVIK KANAKAMEDALA <58741111+SaiRithvik@users.noreply.github.com>
2020-06-04 00:20:42 +0530

    Indexing and assignment of different datatypes

---
+++ datatypes.py
@@ -0,0 +1,27 @@
+
+#Q1 LIST
+
+list1 = ['hi', 'I', 'am', 'Rithvik']  ### assigning list
+
+list2 = [1, 2, 'Hello World!', "Goodbye"] 				  ### assigning list
+
+print(list1[3])  ### List indexing. It goes from 0 to n-1 where n is the length of the list
+print(list2[2])  ### List indexing
+
+
+#Q2 TUPLE
+
+tuple1 = (1,2,"Mycaptain")   ### Initialising a tuple
+
+print(tuple1[1])   ### Indexing a tuple
+
+#Q3 DICTIONARY
+
+dict1 = {'a':1, 'b':2.37, 'c': "Rithvik"}
+
+del dict1['a']  ### Deleting elements of a dictionary
+
+print(dict1)
+
+
+
